c45c2d90148924d347f21f5d9c3953c6039e41ef
Jamo <jamo.jaakola@gmail.com>
2025-11-25 14:02:11 +0200

    Divided to components

--- App.tsx
+++ App.tsx
@@ -1,76 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
-import React, { useEffect, useState } from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
 
-type Todo = {
-  text: string;
-  completed: boolean;
-};
+import useTodos from "./components/useTodos";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-const STORAGE_KEY = 'todos';
 
 export default function App() {
-
-  const [todo, setTodo] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        const json = await AsyncStorage.getItem(STORAGE_KEY);
-        if (json) setTodos(JSON.parse(json));
-      } catch (e) { }
-    })();
-  }, []);
-
-  useEffect(() => {
-    AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = () => {
-    if (todo.trim() === "") return;
-    setTodos([...todos, { text: todo, completed: false }]);
-    setTodo('');
-  };
-
-  const toggleTodo = (index: number) => {
-    const updated = [...todos];
-    updated[index].completed = !updated[index].completed;
-    setTodos(updated);
-  };
+  const { todo, todos, setTodo, addTodo, toggleTodo } = useTodos();
 
   return (
     <View style={styles.container}>
-      <Text>TodoList:</Text>
+      <Text>Todo List</Text>
 
-      <TextInput
-        placeholder='Enter task'
+      <TodoInput
         value={todo}
         onChangeText={setTodo}
-        style={styles.input}
+        onSubmit={addTodo}
       />
 
-      <Button title="Save" onPress={addTodo}/>
-
-      <FlatList
-        data={todos}
-        keyExtractor={(_, index) => index.toString()}
-        renderItem={({ item, index }) => (
-          <Text
-            onPress={() => toggleTodo(index)}
-            style={[
-              styles.todoItem,
-              item.completed && {
-                textDecorationLine: 'line-through',
-                textDecorationStyle: 'solid',
-              }
-            ]}
-          >
-            {index + 1}. {item.text}
-          </Text>
-        )}
-      />
+      <TodoList todos={todos} onToggle={toggleTodo} />
 
       <StatusBar style="auto" />
     </View>
@@ -80,20 +29,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#ccc',
-    width: '80%',
-    padding: 10,
-    marginVertical: 10,
-    borderRadius: 5,
-  },
-  todoItem: {
-    fontSize: 25,
-    marginVertical: 5,
-  }
 });
